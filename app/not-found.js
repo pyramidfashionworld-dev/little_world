5cc10ca1ec7c98aa6db2e42800e37bdcc41270a8
Ujjwal Tiwari <pyramidfashionworld@gmail.com>
2025-10-27 00:23:18 +0530

    Initial commit: Little World project

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>This page does not exist.</p>
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Welcome to Little World!</h2>
+      <p>Your 3D baby clothes showcase will appear here.</p>
+    </div>
+  );
+}
